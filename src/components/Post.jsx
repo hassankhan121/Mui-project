6a7adf5ec100738a66e2c652167950b696102897
Hassan khan <126946515+hassankhan121@users.noreply.github.com>
2023-07-16 12:50:45 +0500

    Add files via upload

---
+++ src/components/Post.jsx
@@ -0,0 +1,58 @@
+import { Favorite, FavoriteBorder, MoreVert, Share } from "@mui/icons-material";
+import {
+  Avatar,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Checkbox,
+  IconButton,
+  Typography,
+} from "@mui/material";
+const Post = () => {
+  return (
+    <Card sx={{ margin: 5 }}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: "Blue" }} aria-label="recipe">
+            H
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVert />
+          </IconButton>
+        }
+        title="Hassan khan"
+        subheader="JUly 15, 2023"
+      />
+      <CardMedia
+        component="img"
+        height="20%"
+        image="https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832_1280.jpg"
+        alt="Paella dish"
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          This impressive paella is a perfect party dish and a fun meal to cook
+          together with your guests. Add 1 cup of frozen peas along with the
+          mussels, if you like.
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <Checkbox
+            icon={<FavoriteBorder />}
+            checkedIcon={<Favorite sx={{ color: "red" }} />}
+          />
+        </IconButton>
+        <IconButton aria-label="share">
+          <Share />
+        </IconButton>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default Post;
